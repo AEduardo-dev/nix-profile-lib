0d315574ff601c8cd990f5bb842879ed8eac4f08
Angel Vega <angel@auterion.com>
2026-01-21 15:59:18 +0100

    fix: remove commands from base profile
    Commands will now be taken from the `flk hook` command, which shall
    allow for them to be present in both direnv and non-direnv setups

--- base_profile.nix
+++ base_profile.nix
@@ -21,30 +21,9 @@
   };
 
   commands = [
-    {
-      name = "refresh";
-      script = ''
-        echo "Refreshing environment..."
-        exec nix develop "$FLK_FLAKE_REF" --impure
-        exit 0
-      '';
-    }
-    {
-      name = "switch";
-      script = ''
-        if [ -z "$1" ]; then
-          echo "Usage: switch <profile-name>"
-          exit 1
-        fi
-        echo "Switching to profile: $1"
-        exec nix develop ".#$1" --impure
-        exit 0
-      '';
-    }
   ];
 
   shellHook = ''
-    echo "✓ Base tools loaded"
   '';
 
   containerConfig = {
